0ba42fd592bdfb3dc0ea8bd5a4789e475b7d75a2
ragheba <rawya.ba02@gmail.com>
2025-11-02 00:37:27 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import os 
+
+def list_files_info(path="."):
+    """print name, extension, and size for files in the given folder."""
+    print("Listing files in current folder:\n")
+    for f in os.listdir(path):
+        if os.path.isfile(f):
+            size = os.path.getsize(f)
+            ext = os.path.splitext(f)[1]
+            print("Name:", f)
+            print("Type:", ext)
+            print("Size:", size)
+            print("-------")
+
+list_files_info(".")            
