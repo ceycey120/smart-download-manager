9cf22c2a536c4792913150092e32fabe34b59309
othmeh <othns24@gmail.com>
2025-11-04 10:34:51 +0300

    Enhance docstrings and added comments in main.py
    Updated docstrings for clarity and improved comments.

--- main.py
+++ main.py
@@ -2,26 +2,31 @@ import os
 import shutil
 
 def list_files_info(path="."):
-    """print name, extension, and size for files in the given folder."""
+    """Print file's name, type, and size in the current folder."""
     print("Listing files in current folder:\n")
+    # Loop for printing each file name, type, and size
     for f in os.listdir(path):
         if os.path.isfile(os.path.join(path, f)):
             size = os.path.getsize(f)
-           ext = os.path.splitext(f)[1] or "(no extension)"
-            print("Name:", f)
-            print("Type:", ext)
-            print("Size:", size)
+            ext = os.path.splitext(f)[1] or "(no extension)"
+            print("Name:", f) # Print file's name
+            print("Type:", ext) # Print file's type
+            print("Size:", size) # Print file's size
             print("-------")
 
 def move_file(source, destination):
+    """Function for moving a file(source) to destinated location(destination)"""
 
+    # Check if 'destination' exist. If not, create the destination directory
     if not os.path.exists(destination):
         os.makedirs(destination)
         print(f"New destination folder {destination} created")
 
     try:
+        # Try to move the file from source to destination
         shutil.move(source, destination)
         print(f"Successfully moved file from {source} to {destination}")
+        # In case error occur during moving operation, these specific error warning will be printed as output
     except FileNotFoundError:
         print(f"Error: File not found")
     except PermissionError:
